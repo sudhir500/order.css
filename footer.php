9a0c1623fe46cff1421f4b94e03ec125308010bb
sudhir k gupta <sudhirgupta.456@gmail.com>
2015-03-21 04:28:05 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,11 @@
+<table border="0" style="  text-align: center;margin: 0px auto;">
+  <tbody>
+    <tr>
+         <td><a href="?var=home">Home</a></td>
+         <td><a href="?var=order">Order page</a></td>
+        <td><a href="?var=contact">Contact</a></td>
+        <td><a href="?var=page">Page</a></td>
+       
+    </tr>
+  </tbody>
+</table>
